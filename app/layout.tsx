d8f07b9ca3a20f3cfbaf4938bc7cd6afc7a63d6b
Aynul <aynulabedino@gmail.com>
2024-03-17 23:45:59 +0600

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'evently',
-  description: 'evently is a platform for event management.',
+  title: 'event-sphere',
+  description: 'event-sphere is a platform for event management.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2023 Evently. All Rights reserved.</p>
+        <p>2024 Event-sphere. All Rights reserved.</p>
       </div>
     </footer>
   )
